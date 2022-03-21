8166d3d6dd6152fb5c0d2f355e3c7a5df07ee639
David Kurniawan <s160819001@student.ubaya.ac.id>
2022-03-21 21:07:54 +0700

    setup fragment and navigation

---
+++ app/src/main/java/dk/ubaya/adv160819001midtermproject/view/MainActivity.kt
@@ -0,0 +1,30 @@
+package dk.ubaya.adv160819001midtermproject.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import dk.ubaya.adv160819001midtermproject.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
+        bottomNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return NavigationUI.navigateUp(navController,drawerLayout) || super.onSupportNavigateUp()
+    }
+}
